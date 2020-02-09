be0a55b6e7483a56aa7d2cac8de303aff5af8466
BilalurRehman-27 <57169660+BilalurRehman-27@users.noreply.github.com>
2020-02-09 12:32:21 -0800

    Clonning new project
    Clonning new projecta

--- src/components/authorRoyaltiesContent.js
+++ src/components/authorRoyaltiesContent.js
@@ -67,7 +67,6 @@ class AuthorRoyaltiesContent extends React.Component {
     }
   }
   render() {
-    const {  } = this.props;
     const { list, pagination, yearList, authorList, loading } = this.state;
     const columns = [
 
